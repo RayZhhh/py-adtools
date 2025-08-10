7c242cda87cd08c7d96ce7564a8ff14270518123
RayZhhh <hey_ray_zhang@icloud.com>
2025-08-10 20:41:08 +0800

    adtools

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setup(
+    name='adtools',
+    version='0.1.0',
+    author='Rui Zhang',
+    author_email='rzhang.cs@gmail.com',
+    description='Useful tools for parsing Python programs for algorithm design.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/RayZhhh/py-adtools',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'Topic :: Scientific/Engineering',
+    ],
+    python_requires='>=3.10',
+    install_requires=[],
+)
