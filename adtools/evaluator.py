7b56ebb8b10d2c7bdd82d1f6e0ecce79c239a695
RayZhhh <hey_ray_zhang@icloud.com>
2025-08-31 16:54:12 +0800

    update

--- adtools/evaluator.py
+++ adtools/evaluator.py
@@ -20,6 +20,8 @@ import traceback
 
 from .py_code import PyProgram
 
+__all__ = ['PyEvaluator', 'PyEvaluatorForBigReturnedObject']
+
 
 class PyEvaluator(ABC):
 
@@ -239,7 +241,171 @@ class PyEvaluator(ABC):
             return None
 
 
-class PyEvaluatorForBigReturnedObjectV1(PyEvaluator):
+class PyEvaluatorForBigReturnedObject(PyEvaluator):
+    def __init__(
+            self,
+            exec_code: bool = True,
+            find_and_kill_children_evaluation_process: bool = False,
+            debug_mode: bool = False,
+            *,
+            join_timeout_seconds: int = 10
+    ):
+        """Evaluator interface for evaluating the Python algorithm program. Override this class and implement
+         'evaluate_program' method, then invoke 'self.evaluate()' or 'self.secure_evaluate()' for evaluation.
+
+         **Note:** This class supports the secure_evaluate to handle very big return object, e.g., Tensors.
+
+         Args:
+             exec_code: Using 'exec()' to execute the program code and obtain the callable functions and classes,
+                 which will be passed to 'self.evaluate_program()'. Set this parameter to 'False' if you are going to
+                 evaluate a Python scripy. Note that if the parameter is set to 'False', the arguments 'callable_...'
+                 in 'self.evaluate_program()' will no longer be affective.
+             find_and_kill_children_evaluation_process: If using 'self.secure_evaluate', kill children processes
+                 when they are terminated. Note that it is suggested to set to 'False' if the evaluation process
+                 does not start new processes.
+             debug_mode: Debug mode.
+             join_timeout_seconds: Timeout in seconds to wait for the process to finish. Kill the process if timeout.
+         """
+        super().__init__(
+            exec_code,
+            find_and_kill_children_evaluation_process,
+            debug_mode,
+            join_timeout_seconds=join_timeout_seconds
+        )
+
+    @abstractmethod
+    def evaluate_program(
+            self,
+            program_str: str,
+            callable_functions_dict: Dict[str, Callable] | None,
+            callable_functions_list: List[Callable] | None,
+            callable_classes_dict: Dict[str, Callable] | None,
+            callable_classes_list: List[Callable] | None,
+            **kwargs
+    ) -> Any:
+        raise NotImplementedError(
+            'Must provide an evaluator for a python program. '
+            'Override this method in a subclass.'
+        )
+
+    def _evaluate_and_put_res_in_manager_dict(
+            self,
+            program_str: str,
+            result_dict: multiprocessing.managers.DictProxy,
+            signal_queue: multiprocessing.Queue,
+            redirect_to_devnull: bool,
+            **kwargs
+    ):
+        """Evaluate and store result in Manager().dict() (for large results)."""
+        if redirect_to_devnull:
+            with open(os.devnull, 'w') as devnull:
+                os.dup2(devnull.fileno(), sys.stdout.fileno())
+                os.dup2(devnull.fileno(), sys.stderr.fileno())
+        try:
+            # Evaluate and get results
+            res = self.evaluate(program_str, **kwargs)
+            # Write results into dict
+            result_dict['result'] = res
+            # Put a signal to queue to inform the parent process the evaluation has done
+            signal_queue.put(('ok', None))
+        except Exception as e:
+            if self.debug_mode:
+                print(traceback.format_exc())
+            # Write results into dict
+            result_dict['result'] = None
+            # Put a signal to queue to inform the parent process the evaluation has terminated
+            signal_queue.put(('error', str(e)))
+
+    def secure_evaluate(
+            self,
+            program: str | PyProgram,
+            timeout_seconds: int | float = None,
+            redirect_to_devnull: bool = False,
+            multiprocessing_start_method: str = 'auto',
+            get_evaluate_time: bool = False,
+            **kwargs
+    ):
+        """Evaluate program in a new process. This enables timeout restriction and output redirection.
+        Args:
+            program: the program to be evaluated.
+            timeout_seconds: return 'None' if the execution time exceeds 'timeout_seconds'.
+            redirect_to_devnull: redirect any output to '/dev/null'.
+            multiprocessing_start_method: start a process using 'fork' or 'spawn'. If set to 'auto',
+                the process will be started using 'fork' with Linux/macOS and 'spawn' with Windows.
+                If set to 'default', there will be no changes to system default.
+            get_evaluate_time: get evaluation time for this program.
+            **kwargs: additional keyword arguments to pass to 'evaluate_program'.
+        Returns:
+            Returns the evaluation results. If the 'get_evaluate_time' is True,
+            the return value will be (Results, Time).
+        """
+        if multiprocessing_start_method == 'auto':
+            # Force macOS and Linux use 'fork' to generate new process
+            if sys.platform.startswith('darwin') or sys.platform.startswith('linux'):
+                multiprocessing.set_start_method('fork', force=True)
+        elif multiprocessing_start_method == 'fork':
+            multiprocessing.set_start_method('fork', force=True)
+        elif multiprocessing_start_method == 'spawn':
+            multiprocessing.set_start_method('spawn', force=True)
+
+        with multiprocessing.Manager() as manager:
+            # Path a dictionary to the evaluation process to get maybe very big return objects
+            result_dict = manager.dict()
+            # Pass a queue to the evaluation process to get signals whether the evaluation terminates
+            signal_queue = multiprocessing.Queue()
+            # Start evaluation process
+            process = multiprocessing.Process(
+                target=self._evaluate_and_put_res_in_manager_dict,
+                args=(str(program), result_dict, signal_queue, redirect_to_devnull),
+                kwargs=kwargs,
+            )
+            evaluate_start_time = time.time()
+            process.start()
+
+            try:
+                if timeout_seconds is not None:
+                    try:
+                        # If there is timeout restriction, we try to get results before timeout
+                        signal = signal_queue.get(timeout=timeout_seconds)
+                    except Empty:
+                        # Evaluation timeout happens, we return 'None' as well as the actual evaluate time
+                        eval_time = time.time() - evaluate_start_time
+                        if self.debug_mode:
+                            print(f'DEBUG: evaluation time exceeds {timeout_seconds}s.')
+                        # Terminate and kill all processes after evaluation
+                        self._kill_process_and_its_children(process)
+                        return (None, eval_time) if get_evaluate_time else None
+                else:
+                    # If there is no timeout restriction, we wait until the evaluation terminates
+                    signal = signal_queue.get()
+
+                # Calculate evaluation time and kill children processes
+                eval_time = time.time() - evaluate_start_time
+                # Terminate and kill all processes after evaluation
+                self._kill_process_and_its_children(process)
+
+                # The first element is 'ok' indicates that the evaluation terminate without exceptions
+                if signal[0] == 'ok':
+                    # We get the evaluation results from 'manager.dict'
+                    result = result_dict.get('result', None)
+                else:
+                    # The evaluation failed for some reason, so we set the result to 'None'
+                    if self.debug_mode:
+                        print(f'DEBUG: child process error: {signal[1]}')
+                    result = None
+            except:
+                # If there is any exception during above procedure, we set the result to None
+                eval_time = time.time() - evaluate_start_time
+                if self.debug_mode:
+                    print(f'DEBUG: exception in manager evaluate:\n{traceback.format_exc()}')
+                # Terminate and kill all processes after evaluation
+                self._kill_process_and_its_children(process)
+                result = None
+
+        return (result, eval_time) if get_evaluate_time else result
+
+
+class PyEvaluatorForBigReturnedObjectV2(PyEvaluator):
 
     def __init__(
             self,
@@ -413,152 +579,5 @@ class PyEvaluatorForBigReturnedObjectV1(PyEvaluator):
                 print(f'DEBUG: exception in shared evaluate:\n{traceback.format_exc()}')
             self._kill_process_and_its_children(process)
             result = None
-
-        return (result, eval_time) if get_evaluate_time else result
-
-
-class PyEvaluatorForBigReturnedObjectV2(PyEvaluator):
-    def __init__(
-            self,
-            exec_code: bool = True,
-            find_and_kill_children_evaluation_process: bool = False,
-            debug_mode: bool = False,
-            *,
-            join_timeout_seconds: int = 10
-    ):
-        super().__init__(
-            exec_code,
-            find_and_kill_children_evaluation_process,
-            debug_mode,
-            join_timeout_seconds=join_timeout_seconds
-        )
-
-    @abstractmethod
-    def evaluate_program(
-            self,
-            program_str: str,
-            callable_functions_dict: Dict[str, Callable] | None,
-            callable_functions_list: List[Callable] | None,
-            callable_classes_dict: Dict[str, Callable] | None,
-            callable_classes_list: List[Callable] | None,
-            **kwargs
-    ) -> Any:
-        raise NotImplementedError(
-            'Must provide an evaluator for a python program. '
-            'Override this method in a subclass.'
-        )
-
-    def _evaluate_and_put_res_in_manager_dict(
-            self,
-            program_str: str,
-            result_dict: multiprocessing.managers.DictProxy,
-            signal_queue: multiprocessing.Queue,
-            redirect_to_devnull: bool,
-            **kwargs
-    ):
-        """Evaluate and store result in Manager().dict() (for large results)."""
-        if redirect_to_devnull:
-            with open(os.devnull, 'w') as devnull:
-                os.dup2(devnull.fileno(), sys.stdout.fileno())
-                os.dup2(devnull.fileno(), sys.stderr.fileno())
-        try:
-            # Evaluate and get results
-            res = self.evaluate(program_str, **kwargs)
-            # Write results into dict
-            result_dict['result'] = res
-            # Put a signal to queue to inform the parent process the evaluation has done
-            signal_queue.put(('ok', None))
-        except Exception as e:
-            if self.debug_mode:
-                print(traceback.format_exc())
-            # Write results into dict
-            result_dict['result'] = None
-            # Put a signal to queue to inform the parent process the evaluation has terminated
-            signal_queue.put(('error', str(e)))
-
-    def secure_evaluate(
-            self,
-            program: str | PyProgram,
-            timeout_seconds: int | float = None,
-            redirect_to_devnull: bool = False,
-            multiprocessing_start_method: str = 'auto',
-            get_evaluate_time: bool = False,
-            **kwargs
-    ):
-        """Evaluate program in a new process. This enables timeout restriction and output redirection.
-        Args:
-            program: the program to be evaluated.
-            timeout_seconds: return 'None' if the execution time exceeds 'timeout_seconds'.
-            redirect_to_devnull: redirect any output to '/dev/null'.
-            multiprocessing_start_method: start a process using 'fork' or 'spawn'. If set to 'auto',
-                the process will be started using 'fork' with Linux/macOS and 'spawn' with Windows.
-                If set to 'default', there will be no changes to system default.
-            get_evaluate_time: get evaluation time for this program.
-            **kwargs: additional keyword arguments to pass to 'evaluate_program'.
-        Returns:
-            Returns the evaluation results. If the 'get_evaluate_time' is True,
-            the return value will be (Results, Time).
-        """
-        if multiprocessing_start_method == 'auto':
-            if sys.platform.startswith('darwin') or sys.platform.startswith('linux'):
-                multiprocessing.set_start_method('fork', force=True)
-        elif multiprocessing_start_method == 'fork':
-            multiprocessing.set_start_method('fork', force=True)
-        elif multiprocessing_start_method == 'spawn':
-            multiprocessing.set_start_method('spawn', force=True)
-
-        with multiprocessing.Manager() as manager:
-            # Path a dictionary to the evaluation process to get maybe very big return objects
-            result_dict = manager.dict()
-            # Pass a queue to the evaluation process to get signals whether the evaluation terminates
-            signal_queue = multiprocessing.Queue()
-            # Start evaluation process
-            process = multiprocessing.Process(
-                target=self._evaluate_and_put_res_in_manager_dict,
-                args=(str(program), result_dict, signal_queue, redirect_to_devnull),
-                kwargs=kwargs,
-            )
-            evaluate_start_time = time.time()
-            process.start()
-
-            try:
-                if timeout_seconds is not None:
-                    try:
-                        # If there is timeout restriction, we try to get results before timeout
-                        signal = signal_queue.get(timeout=timeout_seconds)
-                    except Empty:
-                        # Evaluation timeout happens, we return 'None' as well as the actual evaluate time
-                        eval_time = time.time() - evaluate_start_time
-                        if self.debug_mode:
-                            print(f'DEBUG: evaluation time exceeds {timeout_seconds}s.')
-                        # Terminate and kill all processes after evaluation
-                        self._kill_process_and_its_children(process)
-                        return (None, eval_time) if get_evaluate_time else None
-                else:
-                    # If there is no timeout restriction, we wait until the evaluation terminates
-                    signal = signal_queue.get()
-
-                # Calculate evaluation time and kill children processes
-                eval_time = time.time() - evaluate_start_time
-                # Terminate and kill all processes after evaluation
-                self._kill_process_and_its_children(process)
-
-                # The first element is 'ok' indicates that the evaluation terminate without exceptions
-                if signal[0] == 'ok':
-                    # We get the evaluation results from 'manager.dict'
-                    result = result_dict.get('result', None)
-                else:
-                    # The evaluation failed for some reason, so we set the result to 'None'
-                    if self.debug_mode:
-                        print(f'DEBUG: child process error: {signal[1]}')
-                    result = None
-            except:
-                # If there is any exception during above procedure, we set the result to None
-                eval_time = time.time() - evaluate_start_time
-                if self.debug_mode:
-                    print(f'DEBUG: exception in manager evaluate:\n{traceback.format_exc()}')
-                # Terminate and kill all processes after evaluation
-                self._kill_process_and_its_children(process)
-                result = None
 
         return (result, eval_time) if get_evaluate_time else result
