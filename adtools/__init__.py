ea97be3315e979044c4a5d9e211091ba6e0cc374
RayZhhh <hey_ray_zhang@icloud.com>
2025-12-06 01:13:50 +0800

    update evaluator with shared memory

--- adtools/__init__.py
+++ adtools/__init__.py
@@ -1,3 +1,3 @@
 from .py_code import PyCodeBlock, PyFunction, PyClass, PyProgram
-from .evaluator import PyEvaluator, PyEvaluatorReturnInManagerDict
+from .evaluator import PyEvaluator, PyEvaluatorReturnInManagerDict, PyEvaluatorReturnInSharedMemory
 from .evaluator_pool import EvaluatorExecutorPool
