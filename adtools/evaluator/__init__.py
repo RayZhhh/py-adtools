ea2e613ba92e45b7bb884a0d1a583a7f2cb86bed
RayZhhh <hey_ray_zhang@icloud.com>
2025-12-19 01:05:06 +0800

    update

--- adtools/evaluator/__init__.py
+++ adtools/evaluator/__init__.py
@@ -1,7 +1,6 @@
 from adtools.evaluator.py_evaluator import (
     EvaluationResults,
     PyEvaluator,
-    PyEvaluatorManagerDict,
     PyEvaluatorSharedMemory,
 )
 from adtools.evaluator.py_evaluator_ray import PyEvaluatorRay
